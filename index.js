b0e33e08d415000712f39daeb518a8b07e222159
Quinn Peters <quinn.t.peters@gmail.com>
2016-08-31 18:56:20 -0400

    Changed 'Public' to 'site'

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/site'));
 
 app.get('/', function(request, response) {
-  response.render('public/index.html');
+  response.render('site/index.html');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
 
